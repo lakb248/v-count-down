6653fcdca7c5464592a438f8b0b428f9f8dec462
lakb248 <lakb248@gmail.com>
2016-09-23 10:54:04 +0800

    add unit test

--- test/v-count-down.spec.js
+++ test/v-count-down.spec.js
@@ -3,18 +3,65 @@ import Vue from 'vue';
 import vueCountDown from '../src/v-count-down.vue';
 
 describe('vue-count-down.vue', () => {
-    it('should have correct text', () => {
-        expect(vueCountDown.data().text).toBe('Hello vueCountDown');
-    });
-
-    it('should render correct text', () => {
-        const vm = new Vue({
-            template: '<div><vue-count-down></vue-count-down></div>',
+    it('should call onTimeut on timeout(use duration props)', (done) => {
+        var hasCallTimeout = false;
+        var vm = new Vue({
+            template: '<div><vue-count-down duration="2000" :on-timeout="onTimeout"></vue-count-down></div>',
             components: {
                 'vue-count-down': vueCountDown
+            },
+            methods: {
+                onTimeout() {
+                    hasCallTimeout = true;
+                }
             }
-        }).$mount();
-        expect(vm.$el.querySelector('h2').textContent)
-            .toBe('Hello vueCountDown');
+        }).$mount().$appendTo('body');
+        setTimeout(() => {
+            expect(hasCallTimeout).toBe(true);
+            done();
+        }, 2000);
+    });
+    it('should call onTimeut on timeout(use endTime props)', (done) => {
+        var hasCallTimeout = false;
+        var vm = new Vue({
+            template: '<div><vue-count-down :end-time="endTime" :on-timeout="onTimeout"></vue-count-down></div>',
+            components: {
+                'vue-count-down': vueCountDown
+            },
+            data() {
+                return {
+                    endTime: new Date().getTime() + 2000
+                }
+            },
+            methods: {
+                onTimeout() {
+                    hasCallTimeout = true;
+                }
+            }
+        }).$mount().$appendTo('body');
+        setTimeout(() => {
+            expect(hasCallTimeout).toBe(true);
+            done();
+        }, 2000);
+    });
+    it('should set the duration to zero when endTime is invalid', () => {
+        var hasCallTimeout = false;
+        var vm = new Vue({
+            template: '<div><vue-count-down v-ref:count-down :end-time="endTime" :on-timeout="onTimeout"></vue-count-down></div>',
+            components: {
+                'vue-count-down': vueCountDown
+            },
+            data() {
+                return {
+                    endTime: 'ee'
+                }
+            },
+            methods: {
+                onTimeout() {
+                    hasCallTimeout = true;
+                }
+            }
+        }).$mount().$appendTo('body');
+        expect(vm.$refs.countDown.duration).toBe(0);
     });
 });
